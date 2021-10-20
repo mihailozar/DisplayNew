077fe4b6b88f407ad2debb1a6301009a0c0133a3
mihailozar <70112156+mihailozar@users.noreply.github.com>
2021-10-20 20:20:41 +0200

    libs

---
+++ STM32CubeIDE/Debug/Application/User/TouchGFX/App/app_touchgfx.d
@@ -0,0 +1,5 @@
+Application/User/TouchGFX/App/app_touchgfx.o: \
+ C:/Users/mihailozar/Desktop/DisplayNew/TouchGFX/App/app_touchgfx.c \
+ C:/Users/mihailozar/Desktop/DisplayNew/TouchGFX/App/app_touchgfx.h
+
+C:/Users/mihailozar/Desktop/DisplayNew/TouchGFX/App/app_touchgfx.h:
---
+++ STM32CubeIDE/Debug/Drivers/BSP/Components/ft6x06.d
@@ -0,0 +1,8 @@
+Drivers/BSP/Components/ft6x06.o: \
+ C:/Users/mihailozar/Desktop/DisplayNew/Drivers/BSP/Components/ft6x06/ft6x06.c \
+ C:/Users/mihailozar/Desktop/DisplayNew/Drivers/BSP/Components/ft6x06/ft6x06.h \
+ c:\users\mihailozar\desktop\displaynew\drivers\bsp\components\common\ts.h
+
+C:/Users/mihailozar/Desktop/DisplayNew/Drivers/BSP/Components/ft6x06/ft6x06.h:
+
+c:\users\mihailozar\desktop\displaynew\drivers\bsp\components\common\ts.h:
